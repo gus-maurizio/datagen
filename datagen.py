6516ea7a25c4330d6e8b3acb93e6dcd9c8861be3
gustavo maurizio <gustavo,maurizio@gmail.com>
2018-11-12 09:27:23 -0500

    Mon Nov 12 09:27:22 EST 2018

--- datagen.py
+++ datagen.py
@@ -54,7 +54,7 @@ def main():
 
     args = parser.parse_args()
 
-    print("{} will generate {:,} records of {} bytes at {:.2f} records per second".format(myName, args.number, args.length, args.rate), file=sys.stderr)
+    print("{} will generate {:,} records of {:,} [+/- {:,}]  bytes at {:,.2f} [+/- {:,.2f}] records per second".format(myName, args.number, args.length, args.jitterlen, args.rate, args.jitterrate), file=sys.stderr)
     ### get the actual format string, that will print time in Unix format with nanoseconds 
     formatlen  = args.length - 1
     formatlenj = formatlen
@@ -74,12 +74,12 @@ def main():
         bytecount += formatlenj + 1
         print(formatstr % (time_now))
         if i % max(5, args.number / 50) == 0:
-            progress(i, args.number,       status='{:,d} records @ {:,.2f} rps. Total bytes: {:,d}'.format(i, float(i) / (time_now - time_start), bytecount) )
+            progress(i, args.number,       status='{:,d} records @ {:,.2f} rps. Total bytes: {:,d} avg record length: {:,.2f}'.format(i, float(i) / (time_now - time_start), bytecount, float(bytecount)/float(i+1)) )
         if args.jitterrate != 0:
             high_resolution_sleep(random.uniform(waittime * (1-args.jitterrate/args.rate), waittime * (1+args.jitterrate/args.rate) ))
         else:
             high_resolution_sleep(waittime)
-    progress(args.number, args.number, status='{:,d} records @ {:,.2f} rps. Total bytes: {:,d}'.format(args.number, float(args.number) / (time_now - time_start), bytecount) )
+    progress(args.number, args.number, status='{:,d} records @ {:,.2f} rps. Total bytes: {:,d} avg record length: {:,.2f}'.format(args.number, float(args.number) / (time_now - time_start), bytecount, float(bytecount)/float(args.number)) )
     print("\n%s ended" % (myName), file=sys.stderr)
 
 if __name__ == "__main__":
