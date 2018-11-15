e0eec60ba201cacfa10b96791a9efdc6f8d29476
gustavo maurizio <gustavo,maurizio@gmail.com>
2018-11-15 15:46:36 -0500

    Thu Nov 15 15:46:36 EST 2018

---
+++ testsleep.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println()
+
+	for j := 0; j < 3; j++ {
+		// Determine time.Now() monotonic resolution.
+		if j > 0 {
+			//fmt.Println("time.Sleep(3ms)")
+			//time.Sleep(3 * time.Millisecond)
+		}
+		for i := 0; i < 5; i++ {
+			start := time.Now()
+			time.Sleep(200 * time.Microsecond)
+			stop  := time.Now()
+			fmt.Printf("    time.Now() monotonic resolution:  %5.0fus\n", stop.Sub(start).Seconds()*1e6)
+		}
+	}
+
+}
