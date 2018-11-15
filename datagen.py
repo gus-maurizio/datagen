cb2bc85784672008529bf89f385d28b687b44187
gustavo maurizio <gustavo,maurizio@gmail.com>
2018-11-15 14:06:46 -0500

    Thu Nov 15 14:06:46 EST 2018

--- datagen.py
+++ datagen.py
@@ -22,7 +22,7 @@ def progress(count, total, status=''):
     bar = '=' * filled_len + '-' * (bar_len - filled_len)
 
     sys.stderr.write('>>> [{}] {:6.2f}{} ...{}\r'.format(bar, percents, '%', status))
-    sys.stderr.flush() 
+    sys.stderr.flush()
 
 
 def main():
@@ -30,13 +30,13 @@ def main():
     myName              = os.path.basename(os.path.splitext(sys.argv[0])[0])
     ### set defaults and a more easy to understand command line argument style
     parser = argparse.ArgumentParser(description="datagen utility", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    
+
     parser.add_argument("-n", "--number",
                         default=10,
                         type=int,
                         help="number of records")
     parser.add_argument("-l", "--length",
-                        default=100, 
+                        default=100,
                         type=int,
                         help="record length")
     parser.add_argument("-j", "--jitterlen",
@@ -54,13 +54,13 @@ def main():
 
     args = parser.parse_args()
 
-    print("{} will generate {:,} records of {:,} [+/- {:,}]  bytes at {:,.2f} [+/- {:,.2f}] records per second".format(myName, args.number, args.length, args.jitterlen, args.rate, args.jitterrate), file=sys.stderr)
-    ### get the actual format string, that will print time in Unix format with nanoseconds 
+    print("{} will generate {:,} records of {:,} [+/- {:,}] bytes at {:,.2f} [+/- {:,.2f}] records per second".format(myName, args.number, args.length, args.jitterlen, args.rate, args.jitterrate), file=sys.stderr)
+    ### get the actual format string, that will print time in Unix format with nanoseconds
     formatlen  = args.length - 1
     formatlenj = formatlen
     formatstr = "%0" + str(formatlenj) + "f"
     ### calculate the time between two successive records to be generated based on the record rate specified
-    ### formula is simple, waittime = 1.0 / float(args.rate) 
+    ### formula is simple, waittime = 1.0 / float(args.rate)
     waittime   = 1.0 / args.rate
     ### count the bytes actually sent
     bytecount  = 0
@@ -74,14 +74,13 @@ def main():
         bytecount += formatlenj + 1
         print(formatstr % (time_now))
         if i % max(5, args.number / 50) == 0:
-            progress(i, args.number,       status='{:,d} @{:,.2f} rps. Bytes: {:,d} <{:,.2f}>'.format(i, float(i) / (time_now - time_start), bytecount, float(bytecount)/float(i+1)) )
+            progress(i, args.number,       status='{:,d} @{:,.2f} rps. Bytes: {:,d} <{:,.2f} bytes>'.format(i, float(i) / (time_now - time_start), bytecount, float(bytecount)/float(i+1)) )
         if args.jitterrate != 0:
             high_resolution_sleep(random.uniform(waittime * (1-args.jitterrate/args.rate), waittime * (1+args.jitterrate/args.rate) ))
         else:
             high_resolution_sleep(waittime)
-    progress(args.number, args.number, status='{:,d} @{:,.2f} rps. Bytes: {:,d} <{:,.2f}>'.format(args.number, float(args.number) / (time_now - time_start), bytecount, float(bytecount)/float(args.number)) )
+    progress(args.number, args.number, status='{:,d} @{:,.2f} rps. Bytes: {:,d} <{:,.2f} bytes>'.format(args.number, float(args.number) / (time_now - time_start), bytecount, float(bytecount)/float(args.number)) )
     print("\n%s ended" % (myName), file=sys.stderr)
 
 if __name__ == "__main__":
     main()
-
